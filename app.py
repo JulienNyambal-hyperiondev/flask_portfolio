d77781c8973523863ed953fd019b32000a910f79
Julien Nyambal <Julien.nyambal@gmail.com>
2025-03-22 17:08:40 +0200

    change without restrt

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html', message="Anything as a message")
+
+if __name__ == "__main__":
+    app.run(debug=True)
