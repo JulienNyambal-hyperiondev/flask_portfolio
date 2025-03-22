bbdb2b14d925e7386f0d90c6c60ed99e92ee1082
Julien Nyambal <Julien.nyambal@gmail.com>
2025-03-22 17:37:43 +0200

    added about file

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/about")
+def about():
+    return render_template("about.htm")
+
 @app.route("/")
 def hello_world():
-    return render_template('index.html', message="Anything as a message")
+    return render_template('index.html', message="Intro to Git")
 
 if __name__ == "__main__":
     app.run(debug=True)
